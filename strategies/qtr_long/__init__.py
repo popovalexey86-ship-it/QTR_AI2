6e82ea940ac7b4a0c6bd7508ab51e7b2945223b5
popovalexey86-ship-it <popov.alexey86@gmail.com>
2026-09-03 21:18:49 +0200

    Export QTR Long scoring components

--- strategies/qtr_long/__init__.py
+++ strategies/qtr_long/__init__.py
@@ -6,6 +6,8 @@ entry, but it must never be converted into a short entry.
 
 from strategies.qtr_long.decision_engine import LongDecisionEngine
 from strategies.qtr_long.regime import LongMarketRegime, LongRegimeEngine
+from strategies.qtr_long.score import LongScore, LongScoreGrade
+from strategies.qtr_long.scoring import LongScoringEngine
 from strategies.qtr_long.setup import LongSetupCandidate, LongSetupType
 from strategies.qtr_long.setup_detector import LongSetupDetector
 from strategies.qtr_long.strategy import QTRLongStrategy
@@ -14,6 +16,9 @@ __all__ = [
     "LongDecisionEngine",
     "LongMarketRegime",
     "LongRegimeEngine",
+    "LongScore",
+    "LongScoreGrade",
+    "LongScoringEngine",
     "LongSetupCandidate",
     "LongSetupDetector",
     "LongSetupType",
